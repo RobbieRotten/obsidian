24f08c4ea6f3057b4f7e9185244b6fc24d00e169
Alex <aleharb177@gmail.com>
2026-09-12 15:12:44 +1000

    Disable legacy search preview layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,7 +24,7 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search({ enablePreview: true }),
+    Component.Search({ enablePreview: false }),
     Component.Darkmode(),
     Component.Explorer(),
   ],
@@ -36,7 +36,7 @@ export const defaultListPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search({ enablePreview: true }),
+    Component.Search({ enablePreview: false }),
     Component.Darkmode(),
     Component.Explorer(),
   ],
